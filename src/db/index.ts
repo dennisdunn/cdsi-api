a4c541e78225d0b89c7cc19bf58bab9492fa4d87
Dennis Dunn <ansofive@gmail.com>
2025-12-10 22:03:12 -0500

    Added an index to the db.

---
+++ src/db/index.ts
@@ -0,0 +1,4 @@
+export { default as antigens } from "./antigens.json";
+export { default as schedule } from "./schedule.json";
+export { default as testcases } from "./testcases.json";
+export { default as metadata } from "./metadata.json";
