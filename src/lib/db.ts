2684b94fc3f898677215659e49b3f83c151c431a
= <=>
2026-01-05 19:04:06 -0500

    Generalized the  readDb function.

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,7 @@
 import kebabCase from 'just-kebab-case';
 
 export const readDb = (key, collection) => {
-    key = key == "DTaP/Tdap/Td" ? key.toLowerCase() : key
+    key = key.indexOf('/') > -1 ? key.toLowerCase() : key
     key = kebabCase(key)
     return collection.find(item => item.key == key)
 }
