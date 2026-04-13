f452e0a083a0a3ddc38e9872ffadca7e646b81da
Dennis Dunn <ansofive@gmail.com>
2026-04-12 21:14:05 -0400

    Made the group catalog congruent with the other catalogs.

--- src/db/catalog.ts
+++ src/db/catalog.ts
@@ -6,7 +6,7 @@ import testcases from "./testcases.json";
 import observations from "./observations.json";
 
 export default {
-    groups: groups.map(item => ({ key: item.key, name: item.name })),
+    groups: groups.map(item => ({ name: item.key, desc: item.name })),
     antigens: antigens.map(item => ({ name: item.key, desc: item.name })),
     testcases: testcases.map(item => ({ id: item.key, desc: item.testcaseName })),
     vaccines: vaccines.map(item => ({ cvx: item.key, desc: item.shortDescription })),
